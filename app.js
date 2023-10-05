1c3d21fab97b706f5df40c55c0a5581d15999f86
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2023-10-05 16:27:41 +0100

    final assignment

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Add Product Page</h1>");
+/* app.use((req, res, next) => {
+  console.log("This is the first middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("This is the second middleware");
+  res.send("<h1>Hello to the assignment!</h1>");
+}); */
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Users page!</h1>");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express!</h1>");
+  res.send("<h1>Home page!</h1>");
 });
 
 app.listen(3000);
